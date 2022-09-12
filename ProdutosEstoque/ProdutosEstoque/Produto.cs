f3cf68afe4aa45d7c943f05a9b8b339d3c050b31
Vittor de Aguiar <103087952+vittoraguiar@users.noreply.github.com>
2022-09-12 11:44:20 -0300

    Update Produto.cs

--- ProdutosEstoque/ProdutosEstoque/Produto.cs
+++ ProdutosEstoque/ProdutosEstoque/Produto.cs
@@ -2,28 +2,35 @@ namespace ProdutosEstoque;
 
 public class Produto
 {
-    public string Nome;
-    public double Preco;
-    public int Quantidade;
+    private string _nome;
+    private double _preco;
+    private int _quantidade;
+
+    public Produto(string nome, double preco, int quantidade)
+    {
+        _nome = nome;
+        _preco = preco;
+        _quantidade = quantidade;
+    }
 
     private double ValorTotalEstoque()
     {
-        return Preco * Quantidade;
+        return _preco * _quantidade;
     }
 
     public void AdicionarProdutos(int quantidade)
     {
-        Quantidade += quantidade;
+        _quantidade += quantidade;
     }
 
     public void RemoverProdutos(int quantidade)
     {
-        Quantidade -= quantidade;
+        _quantidade -= quantidade;
     }
 
     public override string ToString()
     {
-        return Nome + ", " + Preco.ToString("C") + ", " + Quantidade
+        return _nome + ", " + _preco.ToString("C") + ", " + _quantidade
                + " unidades. Total: " + ValorTotalEstoque().ToString("C");
     }
     
